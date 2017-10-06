deb0cfa1a8475b9f3d87cd4ae47d7bb93845eb99
Giang Coffee <giang.fet.hut@gmail.com>
2017-10-06 17:50:54 +0700

    add builder

---
+++ src/Viettut/Bundle/WebBundle/Controller/BuilderController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: giang
+ * Date: 10/31/15
+ * Time: 10:42 PM
+ */
+
+namespace Viettut\Bundle\WebBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class BuilderController extends Controller
+{
+    /**
+     * @Route("/builder/{hash}", name="builder_page")
+     * @param $request
+     * @return Response
+     */
+    public function builderAction(Request $request, $hash)
+    {
+        return $this->render('ViettutWebBundle:Builder:index.html.twig');
+    }
+}
